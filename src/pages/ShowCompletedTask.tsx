e332483d0d17ffafa98ea2a98fec2ab365e22d68
mamun-opu <abmamun504@gmail.com>
2022-11-11 21:48:31 +0600

    rename router

--- src/pages/ShowCompletedTask.tsx
+++ src/pages/ShowCompletedTask.tsx
@@ -8,7 +8,7 @@ const ShowCompletedTask = () => {
     <div className="h-screen w-screen pt-20 pb-20 bg-zinc-800">
       <div className="flex justify-end mb-8 mr-8">
         <Link className="text-teal-400" to={"/"}>
-          /Show All Task
+          /show all task
         </Link>
       </div>
       <div className="rounded h-full w-5/6 mx-auto overflow-x-auto overflow-y-scroll bg-stone-300 flex items-center flex-col justify-start">
