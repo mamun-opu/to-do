21f2afd7110354c04c505be89564a7442390c084
mamun-opu <abmamun504@gmail.com>
2022-11-11 22:37:54 +0600

    complete task count

--- src/pages/ShowCompletedTask.tsx
+++ src/pages/ShowCompletedTask.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { TaskContext } from "../App";
 
 const ShowCompletedTask = () => {
-  const { todoList, count } = useContext(TaskContext);
+  const { todoList } = useContext(TaskContext);
+  const result = todoList?.filter((todo) => todo.isCompleted === true).length;
   return (
     <div className="h-screen w-screen pt-20 pb-20 bg-zinc-800">
       <div className="flex justify-end mb-8 mr-8">
@@ -13,9 +14,9 @@ const ShowCompletedTask = () => {
       </div>
       <div className="rounded h-full w-5/6 mx-auto overflow-x-auto overflow-y-scroll bg-stone-300 flex items-center flex-col justify-start">
         <button className="btn btn-block uppercase w-56 md:w-64 lg:w-80 m-8">
-          completed task: {count}
+          completed task: {result}
         </button>
-        {count && count > 0 ? (
+        {result && result > 0 ? (
           <table className="table w-56 md:w-64 lg:w-80">
             <thead>
               <tr>
