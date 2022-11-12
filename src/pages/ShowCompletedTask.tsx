2b4f6ffd0bccd8b097fac9c35c0d9881521d19d7
mamun-opu <abmamun504@gmail.com>
2022-11-12 19:18:06 +0600

    filtered completed task with dropdown select

--- src/pages/ShowCompletedTask.tsx
+++ src/pages/ShowCompletedTask.tsx
@@ -1,10 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { TaskContext } from "../App";
 
 const ShowCompletedTask = () => {
-  const { todoList } = useContext(TaskContext);
+  const { todoList, categoryList } = useContext(TaskContext);
   const result = todoList?.filter((todo) => todo.isCompleted === true).length;
+  const [selecetedCategory, setSelectedCategory] = useState("all");
+
   return (
     <div className="h-screen w-screen pt-20 pb-20 bg-zinc-800">
       <div className="flex justify-end mb-8 mr-8">
@@ -21,22 +23,46 @@ const ShowCompletedTask = () => {
             <thead>
               <tr>
                 <th>Name</th>
-                <th>Category</th>
+                <th>
+                  <select
+                    value={selecetedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className="select select-accent w-full max-w-xs"
+                  >
+                    <option value={"all"}>All Category</option>
+                    {categoryList?.map((category, index) => (
+                      <option key={`${Math.floor(Math.random() * 1001)} + ${index}`} value={category}>{category}</option>
+                    ))}
+                  </select>
+                </th>
               </tr>
             </thead>
             <tbody>
-              {todoList?.map((todo) => {
-                return (
-                  todo.isCompleted && (
-                    <>
-                      <tr>
-                        <td>{todo.name}</td>
-                        <td>{todo.category}</td>
-                      </tr>
-                    </>
-                  )
-                );
-              })}
+              {selecetedCategory === "all"
+                ? todoList?.map((todo) => {
+                    return (
+                      todo.isCompleted && (
+                        <>
+                          <tr>
+                            <td>{todo.name}</td>
+                            <td>{todo.category}</td>
+                          </tr>
+                        </>
+                      )
+                    );
+                  })
+                : todoList?.map((todo) => {
+                  return (
+                    todo.isCompleted && todo.category === selecetedCategory && (
+                      <>
+                        <tr>
+                          <td>{todo.name}</td>
+                          <td>{todo.category}</td>
+                        </tr>
+                      </>
+                    )
+                  );
+                })}
             </tbody>
           </table>
         ) : (
